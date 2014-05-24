71217ba02ea511938d6e7455f341cfc8e74739d3
d-horton <de_horton@hotmail.com>
2014-05-24 11:32:54 +0200

    Finish sign in

--- spec/requests/authentification_pages_spec.rb
+++ spec/requests/authentification_pages_spec.rb
@@ -38,7 +38,7 @@ describe "Authentification" do
       before do
         fill_in "Email", with: user.email.upcase
         fill_in "Password", with: user.password
-        click_button "Sign in "
+        click_button "Sign in"
       end
 
       
@@ -46,6 +46,12 @@ describe "Authentification" do
       it { should have_link('Profile',     href: user_path(user)) }
       it { should have_link('Sign out',    href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
+
+      describe "followed by signout" do
+        before { click_link "Sign out"}
+        it { should have_link('Sign in')}
+      end
+
 
     end
 
