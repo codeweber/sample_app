111a56c5614277a4dd5d2539172c37c9955b6e84
d-horton <de_horton@hotmail.com>
2014-05-29 19:12:39 +0200

    Finish Chapter 9

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -7,6 +7,7 @@ describe "User pages" do
   describe "index" do
 
     let(:user) { FactoryGirl.create(:user) }
+
     before(:each) do
       sign_in user
       visit users_path
@@ -28,7 +29,34 @@ describe "User pages" do
           expect(page).to have_selector('li', text: user.name)
         end
       end
-    end      
+    end
+
+    describe "delete link" do
+
+      it { should_not have_link('delete') }
+
+      describe "as an admin user" do
+        let(:admin) { FactoryGirl.create(:admin) }
+        before do
+          sign_in admin
+          visit users_path
+        end
+        
+        it { should have_link('delete', href: user_path(User.first))}
+        it "should be able to delete another user" do
+          expect do
+            click_link('delete', match: :first)
+          end.to change(User, :count).by(-1)
+        end
+
+        it{ should_not have_link('delete', href: user_path(admin))}
+
+      end
+
+
+    end
+
+      
   end
 
 
