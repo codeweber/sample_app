1f9dfb6a7201cda119b402308c20e8afb6af093e
d-horton <de_horton@hotmail.com>
2014-06-15 17:05:41 +0200

    At 11.3

--- spec/requests/authentification_pages_spec.rb
+++ spec/requests/authentification_pages_spec.rb
@@ -92,6 +92,16 @@ describe "Authentification" do
           before { visit users_path }
           it { should have_title('Sign in') }
         end
+        
+        describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_title('Sign in') }
+        end
 
       end
 
@@ -108,7 +118,17 @@ describe "Authentification" do
         end
       end
 
-      
+      describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          specify { expect(response).to redirect_to(signin_path)  }
+        end
+      end
 
     end
 
